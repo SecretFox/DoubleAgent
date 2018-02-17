ec719e56527cab4927fc8780971685750d9d4413
Markus Kivinen <Markus.Kivinen@gmail.com>
2018-02-17 16:24:46 +0200

    Better log dump and parser

--- id files/ExtractFromClientlog.py
+++ id files/ExtractFromClientlog.py
@@ -1,26 +1,49 @@
 import json
 import re
 
-results = {}
+dragon = {}
+lumie = {}
+templar = {}
+shared = {}
 
 
-def extract_data(i, dat):
-    global results
-    start = dat[i]
+def extract_data(start):
+    global dragon, lumie, templar, shared
+    FactionPattern = "Faction=([a-zA-Z]+)"
     QuestPattern = "QuestID=([0-9]+)"
     TaskPattern = "TaskID=([0-9]+)"
     QuestNamePattern = "QuestName=(.*?) TaskID"
-    quest_ID = re.search(QuestPattern, start).group(1)
-    quest_Name = re.search(QuestNamePattern, start).group(1)
-    task_ID = re.search(TaskPattern, start).group(1)
-    ReportPattern = str(task_ID)+" " + "Report=(.*?)\|\|"
-    report = re.search(ReportPattern, "".join(dat), re.DOTALL).group(1)
-    results[str(quest_ID)] = {
-        'QuestID': quest_ID,
-        'QuestName': quest_Name,
-        'TaskID': task_ID,
-        'Report': report
-    }
+    ReportPattern = "Report=(.*?)\|\|"
+    faction = re.search(FactionPattern, line).group(1)
+    quest_ID = re.search(QuestPattern, line).group(1)
+    quest_Name = re.search(QuestNamePattern, line).group(1)
+    task_ID = re.search(TaskPattern, line).group(1)
+    report = re.search(ReportPattern, line).group(1)
+    if faction == "Lumie":
+        lumie[str(quest_ID)] = {
+            'QuestName': quest_Name,
+            'TaskID': task_ID,
+            'Report': report.replace("&&", "\n")
+        }
+    elif faction == "Shared":
+        shared[str(quest_ID)] = {
+            'QuestName': quest_Name,
+            'TaskID': task_ID,
+            'Report': report.replace("&&", "\n")
+        }
+    elif faction == "Dragon":
+        dragon[str(quest_ID)] = {
+            'QuestID': quest_ID,
+            'QuestName': quest_Name,
+            'TaskID': task_ID,
+            'Report': report.replace("&&", "\n")
+        }
+    elif faction == "Templar":
+        templar[str(quest_ID)] = {
+            'QuestName': quest_Name,
+            'TaskID': task_ID,
+            'Report': report.replace("&&", "\n")
+        }
 
 
 with open("G:\Secret World Legends\ClientLog.txt", encoding='utf-8') as data_file:
@@ -28,9 +51,13 @@ with open("G:\Secret World Legends\ClientLog.txt", encoding='utf-8') as data_fil
     index = 0
     for line in content:
         if "WARNING: Scaleform.DoubleAgent" in line:
-            extract_data(index, content)
-        index += 1
-        print(index)
+            extract_data(line)
 
-with open('data.json', 'w') as outfile:
-    json.dump(results, outfile, sort_keys=True,indent=4)
+with open('IlluminatiMissions.json', 'w') as outfile:
+    json.dump(lumie, outfile, sort_keys=True, indent=4)
+with open('TemplarMissions.json', 'w') as outfile:
+    json.dump(templar, outfile, sort_keys=True, indent=4)
+with open('DragonMissions.json', 'w') as outfile:
+    json.dump(dragon, outfile, sort_keys=True, indent=4)
+with open('SharedMissions.json', 'w') as outfile:
+    json.dump(shared, outfile, sort_keys=True, indent=4)
