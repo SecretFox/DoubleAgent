2c163da89f9070bd359408d38d763186ec9ea94e
Markus Kivinen <Markus.Kivinen@gmail.com>
2018-02-17 14:58:00 +0200

    Python improvements

--- id files/jsonToXML.py
+++ id files/jsonToXML.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-import xml.dom.minidom
 import json
 
 Dragon = {}
@@ -8,47 +7,35 @@ Templar = {}
 
 filelist = ["IlluminatiMissions.json", "DragonMissions.json", "TemplarMissions.json"]
 
-glob_data = {}
+FactionData = {}
 for file in filelist:
     with open(file) as json_file:
-        data = json.load(json_file)
-
-        for key in data:
-            del data[key]["QuestID"]
-            del data[key]["QuestName"]
-            del data[key]["Report"]
-            glob_data[key] = data[key]["TaskID"]
-
+        jsonData = json.load(json_file)
+        for key in jsonData:
+            FactionData[key] = jsonData[key]["TaskID"]
 with open('Factions.json', 'w') as f:
-    json.dump(glob_data, f, indent=4)
+    json.dump(FactionData, f, indent=4)
 
-temp = {}
+SharedData = {}
 with open("SharedMissions.json") as json_file:
     data = json.load(json_file)
     for key in data:
-        temp[key] = data[key]["TaskID"]
-
+        SharedData[key] = data[key]["TaskID"]
 with open('Shared.json', 'w') as f:
-    json.dump(temp, f, indent=4)
+    json.dump(SharedData, f, indent=4)
 
 with open("Factions.json", "r") as f:
     FactionJson = json.load(f)
-
-root = ET.Element('root')
-
-for i in FactionJson:
-    ET.SubElement(root, "Q", tID=FactionJson[i], qID=i)
-tree = ET.ElementTree(root)
-
-tree.write("Faction.xml")
+    root = ET.Element('root')
+    for i in FactionJson:
+        ET.SubElement(root, "Q", tID=FactionJson[i], qID=i)
+    tree = ET.ElementTree(root)
+    tree.write("Faction.xml")
 
 with open("Shared.json", "r") as f:
     FactionJson = json.load(f)
-
-root = ET.Element('root')
-
-for i in FactionJson:
-    ET.SubElement(root, "Q", tID=FactionJson[i], qID=i)
-tree = ET.ElementTree(root)
-
-tree.write("Shared.xml")
+    root = ET.Element('root')
+    for i in FactionJson:
+        ET.SubElement(root, "Q", tID=FactionJson[i], qID=i)
+    tree = ET.ElementTree(root)
+    tree.write("Shared.xml")
